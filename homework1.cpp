864a22d4ced4b7adc0120bbba33a96e509c079f8
Andrew_Gong <48646891+ZianGong@users.noreply.github.com>
2019-11-05 22:31:28 +0800

    Add files via upload

---
+++ homework1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+class Book
+{public:
+	Book(char, double, int);
+	void borrow();
+	void restore();
+	void display();
+ private:
+	 char bookname;
+	 double price;
+	 int number;
+};
+Book::Book(char a, double b, int c)
+{
+	bookname = a;
+	price = b;
+	number = c;
+}
+void Book::borrow()
+{
+	number--;
+}
+void Book::restore()
+{
+	number++;
+}
+void Book::display()
+{
+	cout << bookname <<" "<< price <<" "<< number<<endl;
+}
+int main()
+{
+	Book computer('C', 59.00, 10);
+	Book economics('E', 100.00, 10);
+	computer.borrow();
+	computer.display();
+	economics.restore();
+	economics.display();
+}
+
---
+++ homework2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+class Student
+{
+public:
+	void scoretotalcount(double s);
+	static double sum();
+	static double average();
+private:
+	double score;
+	static double total;
+	static double count;
+};
+void Student::scoretotalcount(double s)
+{
+	score = s;
+	total = total + score;
+	count++;
+}
+double Student::sum()
+{
+	return total;
+}
+double Student::average()
+{
+	return total / count;
+}
+double Student::total = 0;
+double Student::count = 0;
+int main()
+{
+	Student student;
+	int n, i;
+	cout << "学生人数：";
+	cin >> n;
+	for (i = 1; i <= n; i++)
+	{
+		double s;
+		cout << "请输入第" << i << "个学生分数：";
+		cin >> s;
+		student.scoretotalcount(s);
+	}
+	cout << "学生总分：" << student.sum() << endl;
+	cout << "学生平均分：" << student.average() << endl;
+}
